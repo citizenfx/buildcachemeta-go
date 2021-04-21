9becfe08a5fb6a935cfd917a789b4571dd0a44fd
blattersturm <peachypies@protonmail.ch>
2021-04-21 11:01:54 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/citizenfx/buildcachemeta-go
+
+go 1.13
+
+require (
+	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/iafan/cwalk v0.0.0-20210125030640-586a8832a711
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/minio/minio-go/v7 v7.0.10
+	github.com/oligot/go-mod-upgrade v0.6.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20210415154028-4f45737414dc // indirect
+	golang.org/x/sys v0.0.0-20210415045647-66c3f260301c // indirect
+	golang.org/x/term v0.0.0-20210406210042-72f3dc4e9b72 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,311 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"github.com/iafan/cwalk"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+type ContentFile struct {
+	Name           string `xml:"Name,attr"`
+	Size           int64  `xml:"Size,attr"`
+	SHA1Hash       string `xml:"SHA1Hash,attr"`
+	SHA256Hash     string `xml:"SHA256Hash,attr,omitempty"`
+	CompressedSize int64  `xml:"CompressedSize,attr"`
+}
+
+type CacheInfo struct {
+	ContentFiles []*ContentFile `xml:"ContentFile,allowempty"`
+}
+
+func (cf *CacheInfo) Serialize() (string, error) {
+	bytes, err := xml.MarshalIndent(cf, "", "\t")
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	sourcePath := flag.String("source", ".", "source of cache")
+	bucketEndpoint := flag.String("s3-endpoint", "", "s3 endpoint")
+	bucketKeyID := flag.String("s3-key-id", "", "s3 key id")
+	bucketKey := flag.String("s3-key", "", "s3 key")
+	bucketName := flag.String("s3-name", "updates", "s3 name")
+	branchName := flag.String("branch-name", "dummy", "branch name")
+	branchVersion := flag.String("branch-version", "dummy", "branch version")
+	cacheName := flag.String("cache-name", "fivereborn", "cache name")
+	bootstrapExe := flag.String("bootstrap-executable", "CitizenFX.exe", "current bootstrap executable")
+	bootstrapVersion := flag.String("bootstrap-version", "10000000", "current bootstrap version")
+	flag.Parse()
+
+	bootstrapSize := int64(0)
+	bootstrapHash := ""
+
+	cxt := context.Background()
+
+	minioOptions := &minio.Options{
+		Creds:  credentials.NewStaticV4(*bucketKeyID, *bucketKey, ""),
+		Secure: true,
+	}
+
+	minioClient, err := minio.New(*bucketEndpoint, minioOptions)
+
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	files := make(chan *ContentFile, 8192)
+
+	cwalk.NumWorkers = 64
+
+	go func() {
+		err := cwalk.Walk(*sourcePath, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				return nil
+			}
+
+			if filepath.Ext(path) == ".xz" {
+				return nil
+			}
+
+			fpath := filepath.Join(*sourcePath, path)
+			f, err := os.Open(fpath)
+
+			if err != nil {
+				return err
+			}
+
+			defer f.Close()
+
+			h1 := sha1.New()
+			if _, err := io.Copy(h1, f); err != nil {
+				return err
+			}
+
+			f.Seek(0, io.SeekStart)
+
+			h2 := sha256.New()
+			if _, err := io.Copy(h2, f); err != nil {
+				return err
+			}
+
+			f.Close()
+
+			npath := strings.ReplaceAll(path, "\\", "/")
+
+			s256 := h2.Sum(nil)
+
+			exists := false
+			compressed := false
+
+			if info.Size() > 8192 {
+				compressed = true
+			}
+
+			suffix := ""
+
+			if compressed {
+				suffix = ".xz"
+			}
+
+			objectKey := fmt.Sprintf("%x/%x/%x%v", s256[0:1], s256[1:2], s256, suffix)
+
+			remoteInfo, err := minioClient.StatObject(cxt, *bucketName, objectKey, minio.StatObjectOptions{})
+
+			if err == nil {
+				exists = true
+			}
+
+			cSize := info.Size()
+
+			if exists {
+				cSize = remoteInfo.Size
+			}
+
+			if !exists {
+				spath := fpath
+
+				if compressed {
+					cmd := exec.Command("xz", "-zfk", "-T", "0", fpath)
+					err = cmd.Start()
+
+					if err != nil {
+						return err
+					}
+
+					err = cmd.Wait()
+
+					if err != nil {
+						return err
+					}
+
+					cstat, err := os.Stat(fpath + ".xz")
+
+					if err != nil {
+						return err
+					}
+
+					cSize = cstat.Size()
+					spath += ".xz"
+				}
+
+				// function to let defer work
+				err = (func() error {
+					f, err = os.Open(spath)
+
+					if err != nil {
+						return err
+					}
+
+					defer f.Close()
+
+					// upload the object
+					_, err = minioClient.PutObject(cxt, *bucketName, objectKey, f, cSize, minio.PutObjectOptions{
+						ContentType: "application/octet-stream",
+						UserMetadata: map[string]string{
+							"FileName": strings.ReplaceAll(path, "\\", "/"),
+						},
+					})
+
+					return err
+				})()
+
+				if err != nil {
+					return err
+				}
+
+				// remove any .xz we just made
+				os.Remove(fpath + ".xz")
+
+				fmt.Printf("%v -> %v...\n", fpath, objectKey)
+			}
+
+			if bootstrapExe != nil && path == *bootstrapExe {
+				bootstrapHash = fmt.Sprintf("%X", s256)
+				bootstrapSize = cSize
+			} else {
+				files <- &ContentFile{
+					Name:           npath,
+					Size:           info.Size(),
+					SHA1Hash:       fmt.Sprintf("%X", h1.Sum(nil)),
+					SHA256Hash:     fmt.Sprintf("%X", s256),
+					CompressedSize: cSize,
+				}
+			}
+
+			return nil
+		})
+
+		close(files)
+
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+	}()
+
+	infoFile := &CacheInfo{}
+
+	for file := range files {
+		infoFile.ContentFiles = append(infoFile.ContentFiles, file)
+	}
+
+	sort.Slice(infoFile.ContentFiles, func(i, j int) bool {
+		return infoFile.ContentFiles[i].Name < infoFile.ContentFiles[j].Name
+	})
+
+	xmlData, err := infoFile.Serialize()
+
+	if err != nil {
+		return err
+	}
+
+	var manifestHash string
+
+	{
+		h2 := sha256.New()
+		if _, err := h2.Write([]byte(xmlData)); err != nil {
+			return err
+		}
+
+		s256 := h2.Sum(nil)
+		manifestHash = fmt.Sprintf("%x", s256)
+		f := bytes.NewReader([]byte(xmlData))
+
+		objectKey := fmt.Sprintf("%x/%x/%x", s256[0:1], s256[1:2], s256)
+
+		_, err = minioClient.PutObject(cxt, *bucketName, objectKey, f, -1, minio.PutObjectOptions{
+			ContentType: "application/octet-stream",
+			UserMetadata: map[string]string{
+				"FileName": "info.xml",
+			},
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	{
+		_, err = minioClient.PutObject(cxt, *bucketName, fmt.Sprintf("tags/%v/%v/%v", *cacheName, *branchName, *branchVersion), bytes.NewReader([]byte("OK")), -1, minio.PutObjectOptions{
+			UserMetadata: map[string]string{
+				"branch-version":    *branchVersion,
+				"branch-manifest":   manifestHash,
+				"bootstrap-version": *bootstrapVersion,
+				"bootstrap-size":    fmt.Sprintf("%v", bootstrapSize),
+				"bootstrap-object":  bootstrapHash,
+			},
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	{
+		_, err = minioClient.PutObject(cxt, *bucketName, fmt.Sprintf("heads/%v/%v", *cacheName, *branchName), bytes.NewReader([]byte("OK")), -1, minio.PutObjectOptions{
+			UserMetadata: map[string]string{
+				"branch-version":    *branchVersion,
+				"branch-manifest":   manifestHash,
+				"bootstrap-version": *bootstrapVersion,
+				"bootstrap-size":    fmt.Sprintf("%v", bootstrapSize),
+				"bootstrap-object":  bootstrapHash,
+			},
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
